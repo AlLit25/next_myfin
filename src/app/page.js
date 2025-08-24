4d6de40cc7dd8bd3904b06660e03a787e7c3a9d1
Oleksandr Lytvin <allitwin25@gmail.com>
2025-08-24 18:56:53 +0300

    fix img

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import {BASE_PATH} from 'lib/constants';
 
 export default function Home() {
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${BASE_PATH}/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +31,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src={`${BASE_PATH}/vercel.svg`}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +56,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src={`${BASE_PATH}/file.svg`}
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +70,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src={`${BASE_PATH}/window.svg`}
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +84,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src={`${BASE_PATH}/globe.svg`}
             alt="Globe icon"
             width={16}
             height={16}
